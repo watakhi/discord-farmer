73f2f066c9ae3387274f1b356e39a8bde5230845
akhi <87586630+watakhi@users.noreply.github.com>
2024-03-26 02:41:26 +0000

    remove set status

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const { Client } = require('discord.js-selfbot-v13');
 const client = new Client();
 const config = require("./config.json");
-client.user.setPresence({ status: 'invisible' });
 
 client.on("messageCreate", async (message) => {
     if(message.type == "GUILD_MEMBER_JOIN") {
